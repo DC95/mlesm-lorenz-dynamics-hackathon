6f2919790a2a82063274b425e0652dc63d0ae98f
Dwaipayan Chatterjee <57698337+DC95@users.noreply.github.com>
2026-08-14 17:49:39 +0200

    Align starter experiments and evaluation with contract v1.0

--- starter/src/lorenz_hackathon/__init__.py
+++ starter/src/lorenz_hackathon/__init__.py
@@ -1,10 +1,12 @@
 """Scientific starter benchmark for learned Lorenz-63 emulators."""
 
+from .baselines import persistence_rollout
 from .dynamics import LorenzParameters, integrate_lorenz, lorenz_rhs, rk4_step
 
 __all__ = [
     "LorenzParameters",
     "integrate_lorenz",
     "lorenz_rhs",
+    "persistence_rollout",
     "rk4_step",
 ]
